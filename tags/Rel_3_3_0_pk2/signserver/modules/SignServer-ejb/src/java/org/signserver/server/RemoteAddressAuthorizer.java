ad7af2dcc30ace4b4652df42b719c9220b2be8d0
netmackan <netmackan@91c8134f-2a44-0410-ba45-dce2f532dbc6>
2012-11-28 09:56:55 +0000

    Tagging beta

---
+++ tags/Rel_3_3_0_pk2/signserver/modules/SignServer-ejb/src/java/org/signserver/server/RemoteAddressAuthorizer.java
@@ -0,0 +1,120 @@
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.signserver.server;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import javax.persistence.EntityManager;
+import org.apache.log4j.Logger;
+import org.signserver.common.AccessDeniedException;
+import org.signserver.common.AuthorizationRequiredException;
+import org.signserver.common.IllegalRequestException;
+import org.signserver.common.ProcessRequest;
+import org.signserver.common.RequestContext;
+import org.signserver.common.SignServerException;
+import org.signserver.common.WorkerConfig;
+import org.signserver.common.util.XForwardedForUtils;
+import org.signserver.server.log.LogMap;
+
+/**
+ * Authorizer only accepting requests from certain IP addresses.
+ *
+ * Properties:
+ * ALLOW_FROM = Comma separated list of IP addresses to allow requests from.
+ * By default all other addresses are denied access.
+ *
+ * If a worker is invoked directly using an EJB call and no REMOTE_IP is
+ * specified in the RequestContext the IP-address is set to the String "null".
+ * In that case, to allow requests using EJB calls, null can be added to the
+ * list of allowed addresses.
+ *
+ * @author Markus Kilås
+ * @version $Id$
+ */
+public class RemoteAddressAuthorizer implements IAuthorizer {
+
+    /** Logger for this class. */
+    private static final Logger LOG = Logger.getLogger(
+            RemoteAddressAuthorizer.class);
+
+    private static final String PROPERTY_ALLOW_FROM = "ALLOW_FROM";
+
+    private Set<String> allowFrom;
+
+    private int workerId;
+
+    /**
+     * Init this authorizer and loading the list of allowed addresses.
+     *
+     * @param workerId
+     * @param config
+     * @param em
+     * @throws SignServerException
+     */
+    public void init(final int workerId, final WorkerConfig config,
+            final EntityManager em)
+            throws SignServerException {
+        this.workerId = workerId;
+        
+        allowFrom = new HashSet<String>();
+        
+        final String allowFromProperty = config.getProperty(PROPERTY_ALLOW_FROM);
+        
+        if (allowFromProperty != null) {
+            final String[] allowFromStrings = allowFromProperty.split(",");
+            for (String allowFromString : allowFromStrings) {
+                allowFromString = allowFromString.trim();
+                if (allowFromString.length() > 0) {
+                    allowFrom.add(allowFromString);
+                }
+            }
+        }
+    }
+
+    /**
+     * Throws AuthorizationRequiredException unless the requestor's IP address
+     * is allowed.
+     *
+     * @param request
+     * @param requestContext
+     * @throws SignServerException
+     * @throws IllegalRequestException
+     */
+    @Override
+    public void isAuthorized(final ProcessRequest request,
+            final RequestContext requestContext)
+            throws AccessDeniedException, SignServerException, IllegalRequestException {
+
+        String remoteAddress
+                = (String) requestContext.get(RequestContext.REMOTE_IP);
+       
+        if (remoteAddress == null) {
+            remoteAddress = "null";
+        }
+        
+        if (!allowFrom.contains(remoteAddress)) {
+            LOG.error("Worker " + workerId + ": "
+                    + "Not authorized remote address: " + remoteAddress);
+            throw new AccessDeniedException("Remote address not authorized");
+        }
+        
+        LogMap.getInstance(requestContext).put(IAuthorizer.LOG_REMOTEADDRESS, remoteAddress);
+    }
+
+    @Override
+    public List<String> getFatalErrors() {
+        return Collections.emptyList();
+    }
+}
---
+++ tags/Rel_3_3_0_pk2/signserver/modules/SignServer-ejb/src/java/org/signserver/server/archive/olddbarchiver/OldDatabaseArchiver.java
@@ -0,0 +1,140 @@
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.signserver.server.archive.olddbarchiver;
+
+import java.security.cert.X509Certificate;
+import javax.persistence.EntityManager;
+import org.apache.log4j.Logger;
+import org.signserver.common.ArchiveData;
+import org.signserver.common.ArchiveDataVO;
+import org.signserver.common.RequestContext;
+import org.signserver.common.WorkerConfig;
+import org.signserver.common.util.XForwardedForUtils;
+import org.signserver.server.SignServerContext;
+import org.signserver.server.archive.Archivable;
+import org.signserver.server.archive.ArchiveException;
+import org.signserver.server.archive.Archiver;
+import org.signserver.server.archive.ArchiverInitException;
+import org.signserver.server.archive.olddbarchiver.entities.ArchiveDataService;
+import org.signserver.server.log.IWorkerLogger;
+import org.signserver.server.log.LogMap;
+
+/**
+ * Archiver only accepting responses and archiving to the database. 
+ * 
+ * @author Markus Kilås
+ * @version $Id$
+ */
+public class OldDatabaseArchiver implements Archiver {
+    
+    /** Logger for this class. */
+    private static final Logger LOG = Logger.getLogger(OldDatabaseArchiver.class);
+
+    private static final String PROPERTY_ARCHIVE_OF_TYPE = "ARCHIVE_OF_TYPE";
+    private static final String PROPERTY_USE_FORWARDED_ADDRESS = "USE_FORWARDED_ADDRESS";
+    
+    private ArchiveDataService dataService;
+    
+    private ArchiveOfTypes archiveOfTypes;
+
+    private boolean useXForwardedFor = false;
+    
+    @Override
+    public void init(int listIndex, WorkerConfig config, SignServerContext context) throws ArchiverInitException {
+        final EntityManager em = context.getEntityManager();
+        if (em == null) {
+            throw new ArchiverInitException("OldDatabaseArchiver requires a database connection");
+        }
+        dataService = new ArchiveDataService(em);
+        
+        // Configuration of what to archive
+        final String propertyArchiveOfType = "ARCHIVER" + listIndex + "." + PROPERTY_ARCHIVE_OF_TYPE;
+        try {
+            archiveOfTypes = ArchiveOfTypes.valueOf(config.getProperty(propertyArchiveOfType, ArchiveOfTypes.RESPONSE.name()));
+        } catch (IllegalArgumentException ex) {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Illegal value for worker property " + propertyArchiveOfType + ": " + ex.getMessage());
+            }
+            throw new ArchiverInitException("Illegal value for worker property " + propertyArchiveOfType);
+        }
+        
+        // configuration for using the X-FORWARDED-FOR header to determine source IP
+        final String propertyXForwardedFor = "ARCHIVER" + listIndex + "." + PROPERTY_USE_FORWARDED_ADDRESS;
+        useXForwardedFor = Boolean.valueOf(config.getProperty(propertyXForwardedFor));
+    }
+
+    @Override
+    public boolean archive(Archivable archivable, RequestContext requestContext)
+            throws ArchiveException {
+        final boolean archived;
+        
+        // Get the type of this request
+        int archiveType = -1;
+        if (Archivable.TYPE_RESPONSE.equals(archivable.getType())) {
+            archiveType = ArchiveDataVO.TYPE_RESPONSE;
+        } else if (Archivable.TYPE_REQUEST.equals(archivable.getType())) {
+            archiveType = ArchiveDataVO.TYPE_REQUEST;
+        }
+        
+        // Only archive if the type of this request is the type configured for this Archiver
+        if ((archiveOfTypes == ArchiveOfTypes.REQUEST && archiveType == ArchiveDataVO.TYPE_REQUEST)
+                || (archiveOfTypes == ArchiveOfTypes.RESPONSE && archiveType == ArchiveDataVO.TYPE_RESPONSE)
+                || (archiveOfTypes == ArchiveOfTypes.REQUEST_AND_RESPONSE && (archiveType == ArchiveDataVO.TYPE_RESPONSE || archiveType == ArchiveDataVO.TYPE_REQUEST))) {
+            final ArchiveData archiveData;
+            if (archivable instanceof ArchiveDataArchivable) {
+                archiveData = ((ArchiveDataArchivable) archivable).getArchiveData();
+            } else {
+                archiveData = new ArchiveData(archivable.getContentEncoded());
+            }
+            
+            if (dataService == null) {
+                throw new ArchiveException("Could not archive as archiver was not successfully initialized");
+            }
+            final Integer workerId = (Integer) requestContext.get(RequestContext.WORKER_ID);
+            final X509Certificate certificate = (X509Certificate) requestContext.get(RequestContext.CLIENT_CERTIFICATE);
+            String remoteIp = (String) requestContext.get(RequestContext.REMOTE_IP);
+            
+            if (useXForwardedFor) {
+                final String forwardedIp = XForwardedForUtils.getXForwardedForIP(requestContext);
+                
+                if (forwardedIp != null) {
+                    remoteIp = forwardedIp;
+                }
+            }
+
+            final String uniqueId;
+            uniqueId = dataService.create(archiveType,
+                        workerId,
+                        archivable.getArchiveId(),
+                        certificate,
+                        remoteIp,
+                            archiveData);
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Archived with uniqueId: " + uniqueId);
+            }
+            LogMap logMap = LogMap.getInstance(requestContext);
+            String ids = logMap.get(IWorkerLogger.LOG_ARCHIVE_IDS);
+            if (ids == null) {
+                ids = uniqueId;
+            } else {
+                ids = ids + ", " + uniqueId;
+            }
+            logMap.put(IWorkerLogger.LOG_ARCHIVE_IDS, ids);
+            
+            archived = true;
+        } else {
+            archived = false;
+        }
+        return archived;
+    }
+}
