4ac2d508042730c3cd7b3e250be548ebced940cd
malu9369 <malu9369@91c8134f-2a44-0410-ba45-dce2f532dbc6>
2013-02-18 13:01:39 +0000

    Tagging release

---
+++ tags/Rel_3_3_0/signserver/modules/SignServer-Test-System/test/org/signserver/module/mrtdsigner/MRTDSignerTest.java
@@ -0,0 +1,137 @@
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.signserver.module.mrtdsigner;
+
+import java.io.File;
+import java.security.cert.Certificate;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.crypto.Cipher;
+
+import org.signserver.common.GenericSignRequest;
+import org.signserver.common.GenericSignResponse;
+import org.signserver.common.MRTDSignRequest;
+import org.signserver.common.MRTDSignResponse;
+import org.signserver.common.RequestContext;
+import org.signserver.common.SignServerUtil;
+import org.signserver.common.SignerStatus;
+import org.signserver.testutils.ModulesTestCase;
+
+/**
+ * TODO: Document me!
+ * 
+ * @version $Id$
+ */
+public class MRTDSignerTest extends ModulesTestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        SignServerUtil.installBCProvider();
+    }
+
+    /* (non-Javadoc)
+     * @see junit.framework.TestCase#tearDown()
+     */
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void test00SetupDatabase() throws Exception {
+        setProperties(new File(getSignServerHome(), "modules/SignServer-Module-MRTDSigner/src/conf/junittest-part-config.properties"));
+        workerSession.reloadConfiguration(7890);
+    }
+
+    /*
+     * Test method for 'org.signserver.server.MRTDSigner.signData(ISignRequest)'
+     */
+    public void testSignData() throws Exception {
+        int reqid = 12;
+        ArrayList<byte[]> signrequests = new ArrayList<byte[]>();
+
+        byte[] signreq1 = "Hello World".getBytes();
+        byte[] signreq2 = "Hello World2".getBytes();
+        signrequests.add(signreq1);
+        signrequests.add(signreq2);
+
+        MRTDSignResponse res = (MRTDSignResponse) workerSession.process(7890, new MRTDSignRequest(reqid, signrequests), new RequestContext());
+        assertTrue(res != null);
+        assertTrue(reqid == res.getRequestID());
+        Certificate signercert = res.getSignerCertificate();
+        assertNotNull(signercert);
+
+        Cipher c = Cipher.getInstance("RSA", "BC");
+        c.init(Cipher.DECRYPT_MODE, signercert);
+
+        byte[] signres1 = c.doFinal((byte[]) ((ArrayList<?>) res.getProcessedData()).get(0));
+
+        if (!arrayEquals(signreq1, signres1)) {
+            assertTrue("First MRTD doesn't match with request", false);
+        }
+
+        byte[] signres2 = c.doFinal((byte[]) ((ArrayList<?>) res.getProcessedData()).get(1));
+
+        if (!arrayEquals(signreq2, signres2)) {
+            assertTrue("Second MRTD doesn't match with request", false);
+        }
+    }
+
+    private boolean arrayEquals(byte[] signreq2, byte[] signres2) {
+        boolean retval = true;
+
+        if (signreq2.length != signres2.length) {
+            return false;
+        }
+
+        for (int i = 0; i < signreq2.length; i++) {
+            if (signreq2[i] != signres2[i]) {
+                return false;
+            }
+        }
+        return retval;
+    }
+
+    /*
+     * Test method for 'org.signserver.server.MRTDSigner.getStatus()'
+     */
+    public void testGetStatus() throws Exception {
+        SignerStatus stat = (SignerStatus) workerSession.getStatus(7890);
+        assertTrue(stat.getTokenStatus() == SignerStatus.STATUS_ACTIVE);
+
+    }
+
+    public void testGenericSignData() throws Exception {
+
+        int reqid = 13;
+        byte[] signreq1 = "Hello World".getBytes();
+
+        GenericSignResponse res = (GenericSignResponse) workerSession.process(7890, new GenericSignRequest(reqid, signreq1), new RequestContext());
+        assertTrue(res != null);
+        assertTrue(reqid == res.getRequestID());
+        Certificate signercert = res.getSignerCertificate();
+        assertNotNull(signercert);
+
+        Cipher c = Cipher.getInstance("RSA", "BC");
+        c.init(Cipher.DECRYPT_MODE, signercert);
+
+        byte[] signres1 = c.doFinal(res.getProcessedData());
+
+        assertTrue(Arrays.equals(signreq1, signres1));
+    }
+
+    public void test99TearDownDatabase() throws Exception {
+        removeWorker(7890);
+    }
+}
---
+++ tags/Rel_3_3_0/signserver/modules/SignServer-ejb/src/java/org/signserver/statusrepo/impl/StatusRepositorySessionBean.java
@@ -0,0 +1,150 @@
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.signserver.statusrepo.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.ejb.EJBException;
+import javax.ejb.Stateless;
+import org.apache.log4j.Logger;
+import org.signserver.server.log.EventType;
+import org.signserver.server.log.ISystemLogger;
+import org.signserver.server.log.LogMap;
+import org.signserver.server.log.ModuleType;
+import org.signserver.server.log.SystemLoggerException;
+import org.signserver.server.log.SystemLoggerFactory;
+import org.signserver.statusrepo.IStatusRepositorySession;
+import org.signserver.statusrepo.common.NoSuchPropertyException;
+import org.signserver.statusrepo.common.StatusEntry;
+import org.signserver.statusrepo.common.StatusName;
+
+/**
+ * Session bean offering an interface towards the status repository.
+ *
+ * @author Markus Kilås
+ * @version $Id$
+ */
+@Stateless
+public class StatusRepositorySessionBean implements
+        IStatusRepositorySession.ILocal, IStatusRepositorySession.IRemote {
+
+    /** Logger for this class. */
+    private static final Logger LOG =
+            Logger.getLogger(StatusRepositorySessionBean.class);
+
+    /** Audit logger. */
+    private static final ISystemLogger AUDITLOG = SystemLoggerFactory
+            .getInstance().getLogger(StatusRepository.class);
+
+    /** The repository instance. */
+    private final StatusRepository repository;
+
+    
+    /**
+     * Constructs this class.
+     */
+    public StatusRepositorySessionBean() {
+        repository = StatusRepository.getInstance();
+    }
+
+    /**
+     * Get a property.
+     *
+     * @param key Key to get the value for
+     * @return The value if existing and not expired, otherwise null
+     */
+    @Override
+    public StatusEntry getValidEntry(String key) throws NoSuchPropertyException {
+        try {
+            final StatusEntry result;
+            final StatusEntry data = repository.get(StatusName.valueOf(key));
+
+            final long time = System.currentTimeMillis();
+
+            if (data != null && LOG.isDebugEnabled()) {
+                LOG.debug("data.expire=" + data.getExpirationTime() + ", " + time);
+            }
+
+            // First check the expiration and then read the value
+            if (data != null && (data.getExpirationTime() == 0  || data.getExpirationTime() > time)) {
+                result = data;
+            } else {
+                result = null;
+            }
+            return result;
+        } catch (IllegalArgumentException ex) {
+            throw new NoSuchPropertyException(key);
+        }
+    }
+
+    /**
+     * Set a property without expiration, the value will live until the
+     * application is restarted.
+     *
+     * @param key The key to set the value for
+     * @param value The value to set
+     */
+    @Override
+    public void update(final String key, final String value) throws NoSuchPropertyException {
+        update(key, value, 0L);
+    }
+
+     /**
+     * Set a property with a given expiration timestamp.
+     *
+     * After the expiration the get method will return null.
+     *
+     * @param key The key to set the value for
+     * @param value The value to set
+     */
+    @Override
+    public void update(final String key, final String value,
+            final long expiration) throws NoSuchPropertyException {
+        try {
+            final long currentTime = System.currentTimeMillis();
+            repository.set(StatusName.valueOf(key), new StatusEntry(currentTime, value, expiration));
+            auditLog(key, value, expiration);
+        } catch (IllegalArgumentException ex) {
+            throw new NoSuchPropertyException(key);
+        }
+    }
+
+    /**
+     * @return An unmodifiable map of all properties
+     */
+    @Override
+    public Map<String, StatusEntry> getAllEntries() {
+        return repository.getEntries();
+    }
+    
+    private static void auditLog(String property, String value, Long expiration) {
+        try {
+            final LogMap logMap = new LogMap();
+
+            logMap.put(IStatusRepositorySession.LOG_PROPERTY, property);
+            if (value != null) {
+                logMap.put(IStatusRepositorySession.LOG_VALUE,
+                        value);
+            }
+            if (expiration != null) {
+                logMap.put(IStatusRepositorySession.LOG_EXPIRATION,
+                    String.valueOf(expiration));
+            }
+
+            AUDITLOG.log(EventType.SET_STATUS_PROPERTY, ModuleType.STATUS_REPOSITORY, "", logMap);
+        } catch (SystemLoggerException ex) {
+            LOG.error("Audit log failure", ex);
+            throw new EJBException("Audit log failure", ex);
+        }
+    }
+}
