d7e8f3f1cb104cf045a604be050c4a1dce12d01d
netmackan <netmackan@91c8134f-2a44-0410-ba45-dce2f532dbc6>
2013-07-04 14:28:45 +0000

    Tagging beta

---
+++ tags/Rel_3_4_2_pk1/signserver/modules/SignServer-Test-ValidationWS/test/org/signserver/test/validationws/v32/ValidationWSServiceTest.java
@@ -0,0 +1,72 @@
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.signserver.test.validationws.v32;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import org.apache.log4j.Logger;
+
+/**
+ * Test calling ValidationWSService using SignServer 3.2 WSDL.
+ * (The WS end-point URL changed between 3.1 and 3.2.)
+ *
+ * This tests assumes that test-configuration.properties as been applied to
+ * SignServer.
+ *
+ * @version $Id$
+ */
+public class ValidationWSServiceTest extends org.signserver.test.validationws.v31.ValidationWSServiceTest {
+
+    /** Logger for this class. */
+    private static final Logger LOG
+            = Logger.getLogger(ValidationWSServiceTest.class);
+
+    /** Endpoint URL. */
+    private static final String ENDPOINT = "https://localhost:8442/signserver/ValidationWSService/ValidationWS?wsdl";
+
+    public ValidationWSServiceTest(String testName) {
+        super(testName);
+        setupKeystores();
+    }
+
+    /** Setup keystores for SSL. **/
+    private void setupKeystores() {
+        Properties config = new Properties();
+        File confFile1 = new File("../../signserver_build.properties");
+        File confFile2 = new File("../../conf/signserver_build.properties");
+        try {
+            if (confFile1.exists()) {
+                config.load(new FileInputStream(confFile1));
+            } else {
+                config.load(new FileInputStream(confFile2));
+            }
+        } catch (FileNotFoundException ignored) {
+            LOG.debug("No signserver_build.properties");
+        } catch (IOException ex) {
+            LOG.error("Not using signserver_build.properties: " + ex.getMessage());
+        }
+        System.setProperty("javax.net.ssl.trustStore", "../../p12/truststore.jks");
+        System.setProperty("javax.net.ssl.trustStorePassword",
+                config.getProperty("java.trustpassword", "changeit"));
+        //System.setProperty("javax.net.ssl.keyStore", "../../p12/testadmin.jks");
+        //System.setProperty("javax.net.ssl.keyStorePassword", "foo123");
+    }
+
+    @Override
+    protected String getWsEndPointUrl() {
+    	return ENDPOINT;
+    }
+}
