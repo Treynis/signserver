e73284075a37583879bb92a615f285568949cd68
netmackan <netmackan@91c8134f-2a44-0410-ba45-dce2f532dbc6>
2014-01-22 17:15:56 +0000

    DSS-703: Updated paths to libs

--- branches/markus-mvn1/signserver/enable_pom.sh
+++ branches/markus-mvn1/signserver/enable_pom.sh
@@ -12,5 +12,5 @@ done
 ### Install CESeCore jars not in an repository and without POM
 mvn install:install-file -Dfile=lib/ext/cesecore-client-1.1.2.jar -DgroupId=se.primekey.signserver.labs.cesecoremvn -DartifactId=cesecore-client -Dversion=1.1.2 -Dpackaging=jar
 mvn install:install-file -Dfile=lib/ext/cesecore-entity-1.1.2.jar -DgroupId=se.primekey.signserver.labs.cesecoremvn -DartifactId=cesecore-entity -Dversion=1.1.2 -Dpackaging=jar
-
+mvn install:install-file -Dfile=lib/ext/cert-cvc.jar -DgroupId=org.ejbca.cvc -DartifactId=cert-cvc -Dversion=1.2.11 -Dpackaging=jar
 mvn install:install-file -Dfile=lib/ext/xades4j/xades4j-1.3.0-signserver.jar -DgroupId=se.primekey.signserver.labs.xades4j -DartifactId=xades4j -Dversion=1.3.0-signserver -Dpackaging=jar
