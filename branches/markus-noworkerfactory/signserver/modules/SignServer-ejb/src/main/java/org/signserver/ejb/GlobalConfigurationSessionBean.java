4c400fb7fdd5462cbbb6d58ad7fb2ef00bf74875
netmackan <netmackan@91c8134f-2a44-0410-ba45-dce2f532dbc6>
2014-03-15 22:06:28 +0000

    NOWF: Removed caching of GlobalConfiguration

--- branches/markus-noworkerfactory/signserver/modules/SignServer-ejb/src/main/java/org/signserver/ejb/GlobalConfigurationSessionBean.java
+++ branches/markus-noworkerfactory/signserver/modules/SignServer-ejb/src/main/java/org/signserver/ejb/GlobalConfigurationSessionBean.java
@@ -19,7 +19,6 @@ import javax.ejb.EJB;
 import javax.ejb.EJBException;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceException;
 import org.apache.log4j.Logger;
 import org.cesecore.audit.enums.EventStatus;
 import org.cesecore.audit.log.AuditRecordStorageException;
@@ -27,7 +26,6 @@ import org.cesecore.audit.log.SecurityEventsLoggerSessionLocal;
 import org.signserver.common.*;
 import org.signserver.ejb.interfaces.IGlobalConfigurationSession;
 import org.signserver.ejb.worker.impl.IWorkerManagerSessionLocal;
-import org.signserver.server.GlobalConfigurationCache;
 import org.signserver.server.config.entities.FileBasedGlobalConfigurationDataService;
 import org.signserver.server.config.entities.GlobalConfigurationDataBean;
 import org.signserver.server.config.entities.GlobalConfigurationDataService;
@@ -65,7 +63,6 @@ public class GlobalConfigurationSessionBean implements IGlobalConfigurationSessi
     }
 
     private IGlobalConfigurationDataService globalConfigurationDataService;
-    private final GlobalConfigurationCache cache = GlobalConfigurationCache.getInstance();
     
     @PostConstruct
     public void create() {
@@ -102,11 +99,7 @@ public class GlobalConfigurationSessionBean implements IGlobalConfigurationSessi
             String value) {
         auditLog(adminInfo, SignServerEventTypes.SET_GLOBAL_PROPERTY, scope + key, value);
 
-        if (cache.getCurrentState().equals(GlobalConfiguration.STATE_OUTOFSYNC)) {
-            cache.getCachedGlobalConfig().setProperty(propertyKeyHelper(scope, key), value);
-        } else {
-            setPropertyHelper(propertyKeyHelper(scope, key), value);
-        }
+        setPropertyHelper(propertyKeyHelper(scope, key), value);
     }
 
     private String propertyKeyHelper(String scope, String key) {
@@ -135,17 +128,10 @@ public class GlobalConfigurationSessionBean implements IGlobalConfigurationSessi
 
         auditLog(adminInfo, SignServerEventTypes.REMOVE_GLOBAL_PROPERTY, scope + key, null);
 
-        if (cache.getCurrentState().equals(GlobalConfiguration.STATE_OUTOFSYNC)) {
-            cache.getCachedGlobalConfig().remove(propertyKeyHelper(scope, key));
-        } else {
-            try {
-                retval = getGlobalConfigurationDataService().removeGlobalProperty(propertyKeyHelper(scope, key));
-                cache.setCachedGlobalConfig(null);
-            } catch (Throwable e) {
-                LOG.error("Error connecting to database, configuration is un-syncronized", e);
-                cache.setCurrentState(GlobalConfiguration.STATE_OUTOFSYNC);
-                cache.getCachedGlobalConfig().remove(propertyKeyHelper(scope, key));
-            }
+        try {
+            retval = getGlobalConfigurationDataService().removeGlobalProperty(propertyKeyHelper(scope, key));
+        } catch (Throwable e) {
+            LOG.error("Error connecting to database, configuration is un-syncronized", e);
         }
         return retval;
     }
@@ -166,99 +152,41 @@ public class GlobalConfigurationSessionBean implements IGlobalConfigurationSessi
     public GlobalConfiguration getGlobalConfiguration() {
         GlobalConfiguration retval;
 
-        if (cache.getCachedGlobalConfig() == null) {
-            Properties properties = new Properties();
+        Properties properties = new Properties();
 
-            Iterator<GlobalConfigurationDataBean> iter = getGlobalConfigurationDataService().findAll().iterator();
-            while (iter.hasNext()) {
-                GlobalConfigurationDataBean data = iter.next();
-                String rawkey = data.getPropertyKey();
-                String propertyValue = data.getPropertyValue();
-                
-                if (rawkey.startsWith(GlobalConfiguration.SCOPE_NODE)) {
-                    String key = rawkey.replaceFirst(WorkerConfig.getNodeId() + ".", "");
-                    properties.setProperty(key, propertyValue == null ? "" : propertyValue);
+        Iterator<GlobalConfigurationDataBean> iter = getGlobalConfigurationDataService().findAll().iterator();
+        while (iter.hasNext()) {
+            GlobalConfigurationDataBean data = iter.next();
+            String rawkey = data.getPropertyKey();
+            String propertyValue = data.getPropertyValue();
+
+            if (rawkey.startsWith(GlobalConfiguration.SCOPE_NODE)) {
+                String key = rawkey.replaceFirst(WorkerConfig.getNodeId() + ".", "");
+                properties.setProperty(key, propertyValue == null ? "" : propertyValue);
+            } else {
+                if (rawkey.startsWith(GlobalConfiguration.SCOPE_GLOBAL)) {
+                    properties.setProperty(rawkey,
+                                    propertyValue == null ? "" : propertyValue);
                 } else {
-                    if (rawkey.startsWith(GlobalConfiguration.SCOPE_GLOBAL)) {
-                    	properties.setProperty(rawkey,
-                    			propertyValue == null ? "" : propertyValue);
-                    } else {
-                        LOG.error("Illegal property in Global Configuration " + rawkey);
-                    }
+                    LOG.error("Illegal property in Global Configuration " + rawkey);
                 }
             }
-
-            cache.setCachedGlobalConfig(properties);
         }
-        retval = new GlobalConfiguration(cache.getCachedGlobalConfig(), 
-                cache.getCurrentState(), 
+
+        retval = new GlobalConfiguration(properties, 
+                GlobalConfiguration.STATE_INSYNC, 
                 CompileTimeSettings.getInstance().getProperty(CompileTimeSettings.SIGNSERVER_VERSION));
 
         return retval;
     }
 
     /**
+     * @throws org.signserver.common.ResyncException
      * @see org.signserver.ejb.interfaces.IGlobalConfigurationSession.ILocal#resync()
      */
     @Override
     public void resync(final AdminInfo adminInfo) throws ResyncException {
-
-        auditLog(adminInfo, SignServerEventTypes.GLOBAL_CONFIG_RESYNC, null, null); // TODO Should handle errors
-
-        if (!cache.getCurrentState().equals(GlobalConfiguration.STATE_OUTOFSYNC)) {
-            String message = "Error it is only possible to resync a database that have the state " + GlobalConfiguration.STATE_OUTOFSYNC;
-            LOG.error(message);
-            throw new ResyncException(message);
-        }
-        if (cache.getCachedGlobalConfig() == null) {
-            String message = "Error resyncing database, cached global configuration doesn't exist.";
-            LOG.error(message);
-            throw new ResyncException(message);
-        }
-
-        String thisNodeConfig = GlobalConfiguration.SCOPE_NODE + WorkerConfig.getNodeId() + ".";
-        // remove all global and node specific properties
-        try {
-            Collection<GlobalConfigurationDataBean> allProperties = getGlobalConfigurationDataService().findAll();
-            Iterator<GlobalConfigurationDataBean> iter = allProperties.iterator();
-            while (iter.hasNext()) {
-                GlobalConfigurationDataBean data = iter.next();
-                if (data.getPropertyKey().startsWith(GlobalConfiguration.SCOPE_GLOBAL)) {
-                    getGlobalConfigurationDataService().removeGlobalProperty(data.getPropertyKey());
-                } else {
-                    if (data.getPropertyKey().startsWith(thisNodeConfig)) {
-                        getGlobalConfigurationDataService().removeGlobalProperty(data.getPropertyKey());
-                    }
-                }
-            }
-
-        } catch (PersistenceException e) {
-            String message = e.getMessage();
-            LOG.error(message);
-            throw new ResyncException(message);
-        }
-
-        // add all properties
-        Iterator<?> keySet = cache.getCachedGlobalConfig().keySet().iterator();
-        while (keySet.hasNext()) {
-            String fullKey = (String) keySet.next();
-
-            if (fullKey.startsWith(GlobalConfiguration.SCOPE_GLOBAL)) {
-                String scope = GlobalConfiguration.SCOPE_GLOBAL;
-                String key = fullKey.substring(GlobalConfiguration.SCOPE_GLOBAL.length());
-
-                setProperty(scope, key, cache.getCachedGlobalConfig().getProperty(fullKey));
-            } else {
-                if (fullKey.startsWith(GlobalConfiguration.SCOPE_NODE)) {
-                    String scope = GlobalConfiguration.SCOPE_NODE;
-                    String key = fullKey.substring(thisNodeConfig.length());
-                    setProperty(scope, key, cache.getCachedGlobalConfig().getProperty(fullKey));
-                }
-            }
-        }
-
-        // Set the state to insync.
-        cache.setCurrentState(GlobalConfiguration.STATE_INSYNC);
+        throw new UnsupportedOperationException("No longer supported operation");
     }
 
     @Override
@@ -274,11 +202,7 @@ public class GlobalConfigurationSessionBean implements IGlobalConfigurationSessi
         auditLog(adminInfo, SignServerEventTypes.GLOBAL_CONFIG_RELOAD, null, null);
 
         workerManagerSession.flush();
-        cache.setCachedGlobalConfig(null);
         getGlobalConfiguration();
-
-        // Set the state to insync.
-        cache.setCurrentState(GlobalConfiguration.STATE_INSYNC);
     }
     
     @Override
@@ -294,12 +218,9 @@ public class GlobalConfigurationSessionBean implements IGlobalConfigurationSessi
     private void setPropertyHelper(String key, String value) {
         try {
             getGlobalConfigurationDataService().setGlobalProperty(key, value);
-            cache.setCachedGlobalConfig(null);
         } catch (Throwable e) {
             String message = "Error connecting to database, configuration is un-syncronized :";
             LOG.error(message, e);
-            cache.setCurrentState(GlobalConfiguration.STATE_OUTOFSYNC);
-            cache.getCachedGlobalConfig().setProperty(key, value);
         }
 
     }
