e727666c6baba7a5a5b65a86ea68798778615600
malu9369 <malu9369@91c8134f-2a44-0410-ba45-dce2f532dbc6>
2018-06-01 08:00:38 +0000

    DSSINTER-141: Tagging 4.3.0

---
+++ tags/Rel_4_3_0/signserver/bin/signclient.cmd
@@ -0,0 +1,52 @@
+@echo off
+
+rem library classpath
+if "%SIGNSERVER_HOME%" == "" (
+  set SIGNSRV_HOME=..
+  rem It must work to call both as bin\signserver.cmd or from within bin
+  if not exist signclient.cmd set SIGNSRV_HOME=.
+ ) else (
+    set SIGNSRV_HOME=%SIGNSERVER_HOME%
+) 
+
+rem Application server jars
+if not "%APPSRV_HOME%" == "" (
+	set J2EE_CP=%APPSRV_HOME%\lib\jbossall-client.jar;%APPSRV_HOME%\lib\appserv-rt.jar
+)
+
+rem find the JAR
+for /f "tokens=*" %%a in ('dir /b /s %SIGNSRV_HOME%\lib\SignServer-Client-CLI-*.jar') do set JAR=%%a
+
+rem check that we have built the classes
+if not exist %JAR%  (
+    echo You must build SignServer Client CLI first.
+    goto end
+)
+
+rem Optional JARs
+set VALIDATIONCLI_JAR=
+set ENTERPRISE_JAR=
+for /f "tokens=*" %%a in ('dir /b /s %SIGNSRV_HOME%\lib\SignServer-Client-ValidationCLI-*.jar') do set VALIDATIONCLI_JAR=%%a
+for /f "tokens=*" %%a in ('dir /b /s %SIGNSRV_HOME%\lib\SignServer-ClientCLI-Enterprise-*.jar') do set ENTERPRISE_JAR=%%a
+set OPTIONAL_CLASSPATH=%VALIDATIONCLI_JAR%;%ENTERPRISE_JAR%;%EXTRA_CLASSPATH%
+
+set CLASSPATH=%SIGNSRV_HOME%\conf;%SIGNSRV_HOME%\bin;%JAR%;%J2EE_CP%;%SIGNSRV_HOME%\res\deploytools\cesecore;%OPTIONAL_CLASSPATH%
+rem echo %CLASSPATH%
+
+rem Enable Java network debug logs
+rem set JAVA_OPTS="%JAVA_OPTS% -Djavax.net.debug=all"
+rem set JAVA_OPTS="%JAVA_OPTS% -Djavax.net.debug=ssl"
+
+rem Enable Java PKCS#11 debug logs
+rem set JAVA_OPTS="%JAVA_OPTS% -Djava.security.debug=sunpkcs11"
+
+rem Enable Java debugging
+rem set JAVA_OPTS="%JAVA_OPTS% -Xrunjdwp:transport=dt_socket,address=8788,server=y,suspend=n"
+rem set JAVA_OPTS="%JAVA_OPTS% -Xrunjdwp:transport=dt_socket,address=8788,server=y,suspend=y"
+
+if "%JAVA_HOME%" == "" (
+  java %JAVA_OPTS% -cp %CLASSPATH%  org.signserver.client.cli.ClientCLI %*
+) else (
+  "%JAVA_HOME%\bin\java" %JAVA_OPTS% -cp %CLASSPATH% org.signserver.client.cli.ClientCLI %*
+)
+:end
